2af53d4682b3d3f98f44d771ff72d8e59e4dc7bc
Lucas Sagnuinetti <lucas.e.sanguinetti@gmail.com>
2025-12-04 17:24:38 -0300

    se pueden responder preguntas y se muestra si estan o no correctas

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,80 @@ import './App.css'
 import { getQuestions, getDifficulties, getAnswer } from '././services/api';
 
 function App() {
-  const [questions, setQuestions] = useState([])
-  const [index, setindex] = useState(0)
+  const [questions, setQuestions] = useState()
+  const [question, setQuestion] = useState()
+  const [index, setIndex] = useState(0)
+  const [error, setError] = useState()
+  const [isAnswered, setIsAnswered] = useState()
+  const [answer, setAnswer] = useState()
+  const [optionChoosed, setOptionChoosed] = useState()
 
   useEffect(() => {
         getQuestions()
         .then((response) => setTimeout(() => setQuestions(response), 1000))
-        .catch((error) => { setTimeout(() => setError(error), 1000) })  
+        .catch((error) => { setTimeout(() => setError(error), 1000) }) 
     }, []); 
 
-  console.log(questions);
-  
+  useEffect(() => {
+    if(questions){
+      setQuestion(questions[index])
+    }
+  }, [index, questions])
+
+
+  const  handleClick = async (option) => {
+    if (question) {
+      const answer = await getAnswer(question.id, option)
+      setTimeout(() => {
+        setOptionChoosed(option)
+        setIsAnswered(true)
+        setAnswer(answer.answer)
+      }, 500)
+    }
+
+    setTimeout(() => {
+      setIsAnswered(false)
+      setIndex((prev) => prev + 1)
+    }, 1000)
+  }
+
+  const getButtonClass = (option) => {
+            let base = 'button';
+            if (!isAnswered) return base;
+
+            if (option === optionChoosed) {
+                console.log(answer === true)
+                if (answer === true) return `${base} rigthAnswer`;
+                if (answer === false) return `${base} wrongAnswer`;
+            }
+            return `${base} disabled`;
+        };
+
+
+  if (error) return <p>Error al cargar: {error.message}</p>;
+
+  if (!question) {
+  return <p>Cargando preguntas...</p>;
+  }
+
   return (
     <div className="all">
       <div className="containerQuestion">
-        {questions[1].question}
+        {question.question}
       </div>
       <div className="contenedorDeColumnas">
-          <div className="container">
-            {questions[1].option1}
-          </div>
-          <div className="container">
-            {questions[1].option2}
-          </div>
-
-          <div className="container">
-            {questions[1].option3}
-          </div>
-          <div className="container">
-            {questions[1].option4}
-          </div>
+          <button className={getButtonClass("option1")} onClick={() => handleClick("option1")}>
+            {question.option1}
+          </button>
+          <button className={getButtonClass("option2")} onClick={() => handleClick("option2")}>
+            {question.option2}
+          </button>
+          <button className={getButtonClass("option3")} onClick={() => handleClick("option3")}>
+            {question.option3}
+          </button>
+          <button className={getButtonClass("option4")} onClick={() => handleClick("option4")}>
+            {question.option4}
+          </button>
       </div>
     </div>
   )
